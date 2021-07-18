0ebd9fd5c508fc34a3573855113b163d630fd8f6
Yasin <adan.a.yasin@outlook.com>
2021-07-18 16:25:03 -0500

    alain-ai integrated with app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 
 function App() {
 
+  const [newsArticles, setNewsArticles] = useState([])
+
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadLines") {
+          setNewsArticles(articles)
+        }
+      },
+    });
+  }, []);
 
   return (
-    <div className="">
+    <div className="h-screen w-screen border-box overscroll-auto">
       <h1>Alan AI NEWS APP</h1>
     </div>
   );
 }
 
 export default App;
+
+
--- src/Components/NewsCards.js
+++ src/Components/NewsCards.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function NewsCards() {
     return (
-        <div>
-            
+        <div className='grid grid-flow-col grid-cols-3 grid-rows-3 gap-4'>
+            <div className='max-w-sm rounded shadow-lg'>
+                {}
+            </div>
         </div>
     )
 }
