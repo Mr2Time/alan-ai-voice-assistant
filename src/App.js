01ea68eade29d9877b16c42a68b32db69b85502e
Yasin <adan.a.yasin@outlook.com>
2021-07-18 16:14:49 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+function App() {
+
+
+  return (
+    <div className="">
+      <h1>Alan AI NEWS APP</h1>
+    </div>
+  );
+}
+
+export default App;
